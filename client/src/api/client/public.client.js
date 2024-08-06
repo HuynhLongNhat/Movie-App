1ca913d44beb0a168fcafac421d2adb9f8ba7e4e
HuynhLongNhat <nhathuynh227@gmail.com>
2024-08-06 09:35:34 +0700

    update url

--- client/src/api/client/public.client.js
+++ client/src/api/client/public.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://movie-app-server-p47w.onrender.com//api/v1/";
+const baseURL = "https://movie-app-server-p47w.onrender.com/api/v1/";
 
 const publicClient = axios.create({
   baseURL,
